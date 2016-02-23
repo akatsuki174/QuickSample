46aff5ee231e7914b5e26f02d3fc535340961ac9
akatsuki174 <dev2015js@gmail.com>
2016-02-23 13:37:02 +0900

    issue #2 Realmオブジェクトとプレーンなオブジェクトを追加

---
+++ QuickSample/QuickSample/Goods.swift
@@ -0,0 +1,16 @@
+//
+//  Goods.swift
+//  QuickSample
+//
+//  Created by akatsuki174 on 2016/02/23.
+//  Copyright © 2016年 akatsuki174. All rights reserved.
+//
+
+import Foundation
+
+class Goods: NSObject {
+    dynamic var itemNumber = 0
+    dynamic var name = ""
+    dynamic var price = 0
+    dynamic var stock = 0
+}
