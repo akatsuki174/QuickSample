630cf844e97a5cd9703d36136e11f4c9cf3020e9
akatsuki174 <dev2015js@gmail.com>
2016-02-23 19:31:24 +0900

    issue #5 Quickらしくテストクラス名を変更

---
+++ QuickSample/QuickSampleTests/RealmSpec.swift
@@ -0,0 +1,15 @@
+//
+//  RealmSpec.swift
+//  QuickSampleTests
+//
+//  Created by akatsuki174 on 2016/02/23.
+//  Copyright © 2016年 akatsuki174. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+@testable import QuickSample
+
+class RealmSpec: QuickSpec {
+}
