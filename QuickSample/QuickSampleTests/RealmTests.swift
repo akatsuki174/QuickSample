2d2a206934fbd2ae8e55018de54806388ff705a1
akatsuki174 <dev2015js@gmail.com>
2016-02-27 09:17:12 +0900

    issue #8 XCTestを書くクラスを追加

---
+++ QuickSample/QuickSampleTests/RealmTests.swift
@@ -0,0 +1,13 @@
+//
+//  RealmTests.swift
+//  QuickSample
+//
+//  Created by akatsuki174 on 2016/02/27.
+//  Copyright © 2016年 akatsuki174. All rights reserved.
+//
+
+import XCTest
+
+class RealmTests : XCTestCase {
+    
+}
