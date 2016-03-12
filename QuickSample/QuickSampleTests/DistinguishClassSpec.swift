bb930220a02982c05e0ff67d2af928209933a8ed
akatsuki174 <dev2015js@gmail.com>
2016-03-12 22:58:07 +0900

    issue #11 Types/Classesのテストを追加

--- QuickSample/QuickSampleTests/DistinguishClassSpec.swift
+++ QuickSample/QuickSampleTests/DistinguishClassSpec.swift
@@ -11,4 +11,25 @@ import Nimble
 
 class DistinguishClassSpec: QuickSpec {
     
+    override func spec() {
+        describe("make sure a instance class") {
+            let animal = Animal()
+            let cat = Cat()
+            it("can use beAnInstanceOf") {
+                expect(animal).to(beAnInstanceOf(Animal))
+            }
+            it("can use beAKindOf") {
+                expect(cat).to(beAKindOf(Animal))
+                expect(cat).to(beAKindOf(Cat))
+            }
+        }
+    }
+}
+
+class Animal : NSObject {
+    let name: String = ""
+}
+
+class Cat : Animal {
+    
 }
