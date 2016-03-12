cf16cdcb4528d5f5a19a05b02121c85a6325964f
akatsuki174 <dev2015js@gmail.com>
2016-03-12 23:02:36 +0900

    issue #12 Truthinessのテストを書くためのクラスを追加

---
+++ QuickSample/QuickSampleTests/TruthinessSpec.swift
@@ -0,0 +1,14 @@
+//
+//  TruthinessSpec.swift
+//  QuickSample
+//
+//  Created by akatsuki174 on 2016/03/12.
+//  Copyright © 2016年 akatsuki174. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class TruthinessSpec : QuickSpec {
+    
+}
