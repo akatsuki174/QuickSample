cd005f9082acbe82a74c13b07c22f5dd89585bff
akatsuki174 <dev2015js@gmail.com>
2016-03-12 17:33:28 +0900

    issue #10 Comparisonsのテストサンプルを書くためのクラスを追加

---
+++ QuickSample/QuickSampleTests/ComparisonsSpec.swift
@@ -0,0 +1,14 @@
+//
+//  ComparisonsSpec.swift
+//  QuickSample
+//
+//  Created by akatsuki174 on 2016/03/12.
+//  Copyright © 2016年 akatsuki174. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class ComparisonsSpec: QuickSpec {
+    
+}
