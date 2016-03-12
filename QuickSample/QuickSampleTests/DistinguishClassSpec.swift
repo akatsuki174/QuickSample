15906cc3fef46859ab171caeaf748dab1177764a
akatsuki174 <dev2015js@gmail.com>
2016-03-12 22:45:03 +0900

    issue #11 Types/Classesのテストを書くためのクラスを追加

---
+++ QuickSample/QuickSampleTests/DistinguishClassSpec.swift
@@ -0,0 +1,14 @@
+//
+//  DistinguishClassSpec.swift
+//  QuickSample
+//
+//  Created by akatsuki174 on 2016/03/12.
+//  Copyright © 2016年 akatsuki174. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class DistinguishClassSpec: QuickSpec {
+    
+}
