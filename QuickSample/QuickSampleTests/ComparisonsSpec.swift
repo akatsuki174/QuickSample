702879d05205d3b2cc3c05839d2ac59f012845a6
akatsuki174 <dev2015js@gmail.com>
2016-03-12 20:26:58 +0900

    issue #10 「だいたい同じ」を確かめるテストを追加

--- QuickSample/QuickSampleTests/ComparisonsSpec.swift
+++ QuickSample/QuickSampleTests/ComparisonsSpec.swift
@@ -35,6 +35,26 @@ class ComparisonsSpec: QuickSpec {
                 expect(one) >= one
             }
         }
+        
+        describe("make sure if two numbers are almost the same") {
+            it("can use beCloseTo") {
+                expect(five_point_one).to(beCloseTo(five, within: 0.2))
+            }
+            
+            it("can use '≈'") {
+                // default delta is 0.0001
+                expect(5.0001) ≈ five
+                let delta = 0.2
+                expect(five_point_one) ≈ (five, delta)
+                expect(five_point_one) ≈ five ± delta
+                expect(five_point_one) == five ± delta
+            }
+            
+            it("can use array") {
+                expect([0.00001, 2.00001]) ≈ [0.0, 2.0]
+                expect([0.1, 2.1]).to(beCloseTo([0.0, 2.0], within: 0.2))
+            }
+        }
     }
     
 }
