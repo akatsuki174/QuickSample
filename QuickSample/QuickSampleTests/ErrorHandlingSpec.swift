291fa5a54b301517b55223c62ae0bbfdd743c79d
akatsuki174 <dev2015js@gmail.com>
2016-03-13 00:20:03 +0900

    issue #13 エラーハンドリングのテストを書くためのクラスを追加

---
+++ QuickSample/QuickSampleTests/ErrorHandlingSpec.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorHandlingSpec.swift
+//  QuickSample
+//
+//  Created by akatsuki174 on 2016/03/13.
+//  Copyright © 2016年 akatsuki174. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class ErrorHandlingSpec : QuickSpec {
+
+}
