48e6b4f72addf4235906fc1a9571b564b1e76cee
akatsuki174 <dev2015js@gmail.com>
2016-03-13 00:13:55 +0900

    issue #12 Truthinessのテストを追加

--- QuickSample/QuickSampleTests/TruthinessSpec.swift
+++ QuickSample/QuickSampleTests/TruthinessSpec.swift
@@ -10,5 +10,41 @@ import Quick
 import Nimble
 
 class TruthinessSpec : QuickSpec {
-    
+    override func spec() {
+        describe("make sure truthiness") {
+            context("when actual is true") {
+                it("pass with both beTruthy and beTrue") {
+                    let bl:Bool? = true
+                    expect(bl).to(beTruthy())
+                    expect(bl).to(beTrue())
+                }
+            }
+
+            context("when actual is not nil") {
+                it("pass with beTruthy") {
+                    let str:String? = "hoge"
+                    expect(str).to(beTruthy())
+                }
+            }
+            
+            context("when actual is false") {
+                it("pass with both beFalsy and beFalse") {
+                    let bl:Bool? = false
+                    expect(bl).to(beFalsy())
+                    expect(bl).to(beFalse())
+                }
+            }
+
+            context("when actual is nil") {
+                let bl:Bool? = nil
+                it("pass with beFalsy") {
+                    expect(bl).to(beFalsy())
+                }
+                
+                it("pass with beNil") {
+                    expect(bl).to(beNil())
+                }
+            }
+        }
+    }
 }
