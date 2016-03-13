308508b08c4622ab8172ac50f88a8e4eb031e6f3
akatsuki174 <dev2015js@gmail.com>
2016-03-13 09:34:53 +0900

    issue #14 Collection Membershipのテストを追加

--- QuickSample/QuickSampleTests/CollectionMembershipSpec.swift
+++ QuickSample/QuickSampleTests/CollectionMembershipSpec.swift
@@ -11,4 +11,25 @@ import Nimble
 
 class CollectionMembershipSpec : QuickSpec {
 
+    override func spec() {
+        describe("collection membership") {
+            context("when array is empty") {
+                it("pass with beEmpty") {
+                    let emptyArr:[String] = []
+                    expect(emptyArr).to(beEmpty())
+                }
+            }
+            context("when array is not empty") {
+                let arr:[String]? = ["Swift", "Objective-C", "Python"]
+                it("confirm array elements with contain()") {
+                    expect(arr).to(contain("Swift", "Objective-C"))
+                }
+                
+                it("can check first and last elements") {
+                    expect(arr).to(beginWith("Swift"));
+                    expect(arr).to(endWith("Python"));
+                }
+            }
+        }
+    }
 }
